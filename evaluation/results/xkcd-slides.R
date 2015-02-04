3b945633d0fb0b926e78506aea9568b8cf8cfe00
Jeremy Blackburn <jeremy.blackburn@gmail.com>
2015-02-04 10:16:34 +0100

    more xkcd slides and png versions of them

--- evaluation/results/xkcd-slides.R
+++ evaluation/results/xkcd-slides.R
@@ -52,6 +52,7 @@ pdf("xkcd-slides/time-to-first-byte-per-context-xkcd.pdf", 8, 5)
 p +
   geom_point(mapping=aes(x=X, y=Y, color=protocol)) +
   geom_line(mapping=aes(x=X, y=Y, color=protocol)) +
+  geom_errorbar(mapping=aes(x=X, ymin=Y-stddev, ymax=Y+stddev, color=protocol, width=0.2)) +
 #  geom_smooth(mapping=aes(x=X, y=Y, color=protocol), se=FALSE) +
   theme_xkcd() +
   annotate("text", x=9.25, y=493, label="At 10 contexts, data\nfrom the middlebox\nexceeds 1 MSS", family="xkcd", size=3) +
@@ -64,5 +65,81 @@ p +
   xkcdman(mapping, dataman)
 
 dev.off()
-#p <- ggplot() + geom_smooth(mapping=aes(x=year, y =prev, colour=as.character(country)),
-#                            ,se=FALSE, data =data, method="loess")
+
+
+
+t <- read.table("connections_slice_local_mbox_54-76-148-166.pdf.tsv", header=T, sep="\t")
+
+ratioxy <- diff(range(t$X)) / diff(range(t$Y))
+
+dataman <- data.frame(
+  # x= c(7,12), y=c(450, 525),
+  x= c(8.25), y=c(1100),
+  scale = 150.25,
+  ratioxy = ratioxy,
+  angleofspine =  -pi/2,
+  anglerighthumerus = c(-pi/6),
+  anglelefthumerus = c(-pi/2 - pi/6),
+  anglerightradius = c(-pi/5),
+  angleleftradius = c(-pi/5),
+  angleleftleg = 3*pi/2  + pi / 12,
+  anglerightleg = 3*pi/2  - pi / 12,
+  angleofneck = c(runif(1, 3*pi/2-pi/10, 3*pi/2+pi/10))
+)
+
+p <- ggplot(data=t)
+
+pdf("xkcd-slides/connections-per-second-num-contexts-mbox-xkcd.pdf", 8, 5)
+p +
+  geom_point(mapping=aes(x=X, y=Y, color=protocol)) +
+  geom_line(mapping=aes(x=X, y=Y, color=protocol)) +
+  geom_errorbar(mapping=aes(x=X, ymin=Y-stddev, ymax=Y+stddev, color=protocol, width=0.2)) +
+  #  geom_smooth(mapping=aes(x=X, y=Y, color=protocol), se=FALSE) +
+  theme_xkcd() +
+  annotate("text", x=8.25, y=1500, label="mcTLS actually improves middlebox performance!", family="xkcd", size=5) +
+  theme(legend.position="top") +
+  # xlim(c(0, 16)) +
+  xkcdaxis(range(t$X), range(t$Y)) +
+  ylab("Middlebox Connections per second") +
+  xlab("Number of Contexts") +
+  xkcdman(mapping, dataman)
+
+dev.off()
+
+t <- read.table("connections_slice_local_server_54-76-148-166.pdf.tsv", header=T, sep="\t")
+
+ratioxy <- diff(range(t$X)) / diff(range(t$Y))
+
+dataman <- data.frame(
+  # x= c(7,12), y=c(450, 525),
+  x= c(8.25), y=c(470),
+  scale = 15.25,
+  ratioxy = ratioxy,
+  angleofspine =  -pi/2,
+  anglerighthumerus = c(-pi/6),
+  anglelefthumerus = c(-pi/2 - pi/6),
+  anglerightradius = c(-pi/5),
+  angleleftradius = c(-pi/5),
+  angleleftleg = 3*pi/2  + pi / 12,
+  anglerightleg = 3*pi/2  - pi / 12,
+  angleofneck = c(runif(1, 3*pi/2-pi/10, 3*pi/2+pi/10))
+)
+
+p <- ggplot(data=t)
+
+pdf("xkcd-slides/connections-per-second-num-contexts-server-xkcd.pdf", 8, 5)
+p +
+  geom_point(mapping=aes(x=X, y=Y, color=protocol)) +
+  geom_line(mapping=aes(x=X, y=Y, color=protocol)) +
+  geom_errorbar(mapping=aes(x=X, ymin=Y-stddev, ymax=Y+stddev, color=protocol, width=0.2)) +
+  #  geom_smooth(mapping=aes(x=X, y=Y, color=protocol), se=FALSE) +
+  theme_xkcd() +
+  annotate("text", x=8.25, y=500, label="More contexts or middleboxes only degrades\nserver performance a little bit.", family="xkcd", size=5) +
+  theme(legend.position="top") +
+  # xlim(c(0, 16)) +
+  xkcdaxis(range(t$X), range(t$Y)) +
+  ylab("Server Connections per second") +
+  xlab("Number of Contexts") +
+  xkcdman(mapping, dataman)
+
+dev.off()
